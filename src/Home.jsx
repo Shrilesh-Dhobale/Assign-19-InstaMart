a361395a796e2942177860b418b5af2099ac37fe
dshrileshdhobale@gmail.com <dshrilesh@gmail.com>
2025-11-12 20:42:01 +0530

    Var created for categories

---
+++ src/Home.jsx
@@ -0,0 +1,18 @@
+import React from 'react'
+
+function Home() {
+  const InstamartHome = () => {
+        const categories = [
+        { name: 'Vegetables', icon: '🥬' },
+        { name: 'Dairy', icon: '🥛' },
+        { name: 'Snacks', icon: '🍿' },
+        { name: 'Beverages', icon: '🥤' }
+  ];    }
+  return (
+    <div>
+      
+    </div>
+  )
+}
+
+export default Home
