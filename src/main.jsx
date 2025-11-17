c32b4eacc41642cb956ba1be17b04f9372d677fc
dshrileshdhobale@gmail.com <dshrilesh@gmail.com>
2025-11-17 18:14:30 +0530

    Contact comp added in Route path

--- src/main.jsx
+++ src/main.jsx
@@ -6,6 +6,7 @@ import About from './About.jsx'
 import Navbar from './Navbar.jsx'
 import React from 'react'
 import {BrowserRouter,Routes,Route} from 'react-router-dom'
+import { Contact } from 'lucide-react'
 
 const root = createRoot(document.getElementById('root'));
 
@@ -15,7 +16,7 @@ root.render(
         <Routes>
             <Route path='/' element={<Home/>}/>
             <Route path='/about' element={<About/>}/>
-            <Route path='/contact' element={}/>
+            <Route path='/contact' element={<Contact />}/>
         </Routes>
     </BrowserRouter>
     )
