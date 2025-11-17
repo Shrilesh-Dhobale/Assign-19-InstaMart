da23355f7bdd993ff2045df33e37eaf4c607b5b3
dshrileshdhobale@gmail.com <dshrilesh@gmail.com>
2025-11-17 18:06:04 +0530

    Link for contact

--- src/Navbar.jsx
+++ src/Navbar.jsx
@@ -12,6 +12,9 @@ function Navbar() {
         <li>
           <Link to="/about">About</Link>
         </li>
+        <li>
+          <Link to="/contact">Contact</Link>
+        </li>
       </ul>
     </nav>
   );
