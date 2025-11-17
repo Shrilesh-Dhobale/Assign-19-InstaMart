ab9da52a39408fa20035a18fc1134056f2d34208
dshrileshdhobale@gmail.com <dshrilesh@gmail.com>
2025-11-17 17:55:56 +0530

    Route also imported

--- src/main.jsx
+++ src/main.jsx
@@ -3,7 +3,7 @@ import { createRoot } from 'react-dom/client'
 import './index.css'
 import Home from './Home.jsx'
 import React from 'react'
-import {BrowserRouter,Routes} from 'react-router'
+import {BrowserRouter,Routes,Route} from 'react-router'
 
 createRoot(document.getElementById('root'));
 
